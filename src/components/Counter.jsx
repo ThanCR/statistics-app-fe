62481427fc2c78567fd29cf1149777bbfcde4d60
Jonnathan <jonzxz12@gmail.com>
2024-02-09 15:45:47 -0600

    implementing API use, created fetchapi GET method to register interactions with the button

---
+++ src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import './Counter.css';
+
+function Counter() {
+  return (
+    <>
+        <h2>Counter component</h2>
+        <p className='counter'>0</p>
+    </>
+  );
+}
+
+export default Counter;
---
+++ src/views/Home.jsx
@@ -0,0 +1,15 @@
+import Button from '../components/Button'
+import Counter from '../components/Counter'
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+        <h2>HomeComponent</h2>
+        <Counter/>
+        <Button/>
+    </>
+  );
+}
+
+export default Home;
